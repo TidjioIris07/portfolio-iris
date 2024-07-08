9297f9ff51ccc09fe4f8f6ac6b39254c3a69f5bd
Iris RT <iristidjio@gmail.com>
2024-07-08 18:07:32 +0300

    initial commit

---
+++ components/Socials.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import Link from 'next/link';
+
+const social = [
+    { icon: <FaGithub />, path: "https://github.com/TidjioIris07" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/r-tidjio-iris-retsofa-989a88316/" },
+];
+
+const Socials = ({ containerStyles, iconStyles }) => {
+    return (
+        <div className={containerStyles}>
+            {social.map((item, index) => {
+                return (
+                    <Link key={index} href={item.path} className={iconStyles}>
+                        {item.icon}
+                    </Link>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Socials;
